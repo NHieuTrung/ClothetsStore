8e6d270bbc5c2cfd3d4d0e54de6d45279feee4e8
Trung <hieu.trung.030197@gmail.com>
2019-09-28 14:15:30 +0700

    Fix "Navigation Component"

--- Projects/front-end/public/assets/js/main.js
+++ Projects/front-end/public/assets/js/main.js
@@ -7,7 +7,7 @@
     catList = $('#responsive-nav .category-nav .category-list'),
     menuToggle = $('#responsive-nav .menu-nav .menu-header'),
     menuList = $('#responsive-nav .menu-nav .menu-list');
-
+    
   catToggle.on('click', function() {
     menuList.removeClass('open');
     catList.toggleClass('open');
