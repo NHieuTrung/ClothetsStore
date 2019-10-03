9f060100f5150dccb0b90e199c490f1e1c7b9cfb
HeoBayMau <hieu.trung.030197@gmail.com>
2019-10-03 21:35:25 +0700

    Add files via upload

---
+++ Projects/back-end/Models/ClothetsStoreContext.cs
@@ -0,0 +1,486 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Models
+{
+    public partial class ClothetsStoreContext : DbContext
+    {
+        public ClothetsStoreContext()
+        {
+        }
+
+        public ClothetsStoreContext(DbContextOptions<ClothetsStoreContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Account> Account { get; set; }
+        public virtual DbSet<Brand> Brand { get; set; }
+        public virtual DbSet<Color> Color { get; set; }
+        public virtual DbSet<Customer> Customer { get; set; }
+        public virtual DbSet<Delivery> Delivery { get; set; }
+        public virtual DbSet<Employee> Employee { get; set; }
+        public virtual DbSet<Gender> Gender { get; set; }
+        public virtual DbSet<ImportOrder> ImportOrder { get; set; }
+        public virtual DbSet<Order> Order { get; set; }
+        public virtual DbSet<OrderProductSize> OrderProductSize { get; set; }
+        public virtual DbSet<Product> Product { get; set; }
+        public virtual DbSet<ProductColor> ProductColor { get; set; }
+        public virtual DbSet<ProductGender> ProductGender { get; set; }
+        public virtual DbSet<ProductSize> ProductSize { get; set; }
+        public virtual DbSet<Role> Role { get; set; }
+        public virtual DbSet<Size> Size { get; set; }
+        public virtual DbSet<Status> Status { get; set; }
+        public virtual DbSet<TypeProduct> TypeProduct { get; set; }
+        public virtual DbSet<TypeSize> TypeSize { get; set; }
+        public virtual DbSet<Vendor> Vendor { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-HEOBAYsM;Database=ClothetsStore;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>(entity =>
+            {
+                entity.Property(e => e.AccountId).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Username)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Role)
+                    .WithMany(p => p.Account)
+                    .HasForeignKey(d => d.RoleId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Account_Role");
+            });
+
+            modelBuilder.Entity<Brand>(entity =>
+            {
+                entity.Property(e => e.BrandId).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.CompanyName).HasMaxLength(100);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Color>(entity =>
+            {
+                entity.Property(e => e.ColorId).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.ColorValue)
+                    .IsRequired()
+                    .HasMaxLength(7)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.Property(e => e.CustomerId).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.Address)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Birthday).HasColumnType("date");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Phone)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Account)
+                    .WithMany(p => p.Customer)
+                    .HasForeignKey(d => d.AccountId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Customer_Account");
+
+                entity.HasOne(d => d.Gender)
+                    .WithMany(p => p.Customer)
+                    .HasForeignKey(d => d.GenderId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Customer_Gender");
+            });
+
+            modelBuilder.Entity<Delivery>(entity =>
+            {
+                entity.Property(e => e.DeliveryId).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.Email).HasMaxLength(50);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Phone)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.EmployeeId).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.Address)
+                    .IsRequired()
+                    .HasMaxLength(200);
+
+                entity.Property(e => e.Birthday)
+                    .HasColumnName("BIrthday")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Phone)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Account)
+                    .WithMany(p => p.Employee)
+                    .HasForeignKey(d => d.AccountId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Employee_Account");
+
+                entity.HasOne(d => d.Gender)
+                    .WithMany(p => p.Employee)
+                    .HasForeignKey(d => d.GenderId)
+                    .HasConstraintName("FK_Employee_Gender");
+
+                entity.HasOne(d => d.Status)
+                    .WithMany(p => p.Employee)
+                    .HasForeignKey(d => d.StatusId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Employee_Status");
+            });
+
+            modelBuilder.Entity<Gender>(entity =>
+            {
+                entity.Property(e => e.GenderId).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(20);
+            });
+
+            modelBuilder.Entity<ImportOrder>(entity =>
+            {
+                entity.Property(e => e.ImportOrderId).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.ImportDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 0)");
+
+                entity.HasOne(d => d.Status)
+                    .WithMany(p => p.ImportOrder)
+                    .HasForeignKey(d => d.StatusId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ImportOrder_Status");
+
+                entity.HasOne(d => d.Vendor)
+                    .WithMany(p => p.ImportOrder)
+                    .HasForeignKey(d => d.VendorId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ImportOrder_Vendor");
+
+                entity.HasOne(d => d.ProductSize)
+                    .WithMany(p => p.ImportOrder)
+                    .HasForeignKey(d => new { d.SizeId, d.ProductId, d.ColorId })
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ImportOrder_ProductSize");
+            });
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.Property(e => e.OrderId).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.ContactPhone)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.DeliveryAddress).HasColumnType("decimal(18, 0)");
+
+                entity.Property(e => e.DeliveryDate).HasColumnType("datetime");
+
+                entity.Property(e => e.DeliveryPrice).HasColumnType("decimal(18, 0)");
+
+                entity.Property(e => e.TotalPrice).HasColumnType("decimal(18, 0)");
+
+                entity.HasOne(d => d.Customer)
+                    .WithMany(p => p.Order)
+                    .HasForeignKey(d => d.CustomerId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Order_Customer");
+
+                entity.HasOne(d => d.Delivery)
+                    .WithMany(p => p.Order)
+                    .HasForeignKey(d => d.DeliveryId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Order_Delivery");
+
+                entity.HasOne(d => d.Status)
+                    .WithMany(p => p.Order)
+                    .HasForeignKey(d => d.StatusId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Order_Status");
+            });
+
+            modelBuilder.Entity<OrderProductSize>(entity =>
+            {
+                entity.HasKey(e => new { e.OrderId, e.SizeId, e.ProductId, e.ColorId });
+
+                entity.ToTable("Order_ProductSize");
+
+                entity.Property(e => e.OrderId).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 0)");
+
+                entity.HasOne(d => d.Order)
+                    .WithMany(p => p.OrderProductSize)
+                    .HasForeignKey(d => d.OrderId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Order_ProductSize_Order");
+
+                entity.HasOne(d => d.ProductSize)
+                    .WithMany(p => p.OrderProductSize)
+                    .HasForeignKey(d => new { d.SizeId, d.ProductId, d.OrderId })
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Order_ProductSize_ProductSize");
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.ProductId).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.Code)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Detail)
+                    .IsRequired()
+                    .HasMaxLength(600);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 0)");
+
+                entity.HasOne(d => d.Brand)
+                    .WithMany(p => p.Product)
+                    .HasForeignKey(d => d.BrandId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Product_Brand");
+
+                entity.HasOne(d => d.Status)
+                    .WithMany(p => p.Product)
+                    .HasForeignKey(d => d.StatusId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Product_Status");
+
+                entity.HasOne(d => d.TypeProduct)
+                    .WithMany(p => p.Product)
+                    .HasForeignKey(d => d.TypeProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Product_TypeProduct");
+            });
+
+            modelBuilder.Entity<ProductColor>(entity =>
+            {
+                entity.HasKey(e => new { e.ProductId, e.ColorId });
+
+                entity.Property(e => e.ProductId).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.ImageUrl)
+                    .IsRequired()
+                    .HasMaxLength(300);
+
+                entity.HasOne(d => d.Color)
+                    .WithMany(p => p.ProductColor)
+                    .HasForeignKey(d => d.ColorId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ProductColor_Color");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.ProductColor)
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ProductColor_Product");
+
+                entity.HasOne(d => d.Status)
+                    .WithMany(p => p.ProductColor)
+                    .HasForeignKey(d => d.StatusId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ProductColor_Status");
+            });
+
+            modelBuilder.Entity<ProductGender>(entity =>
+            {
+                entity.Property(e => e.ProductGenderId).ValueGeneratedNever();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(10);
+
+                entity.HasOne(d => d.Status)
+                    .WithMany(p => p.ProductGender)
+                    .HasForeignKey(d => d.StatusId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ProductGender_Status");
+            });
+
+            modelBuilder.Entity<ProductSize>(entity =>
+            {
+                entity.HasKey(e => new { e.SizeId, e.ProductId, e.ColorId });
+
+                entity.Property(e => e.SizeId).HasDefaultValueSql("(newid())");
+
+                entity.HasOne(d => d.Size)
+                    .WithMany(p => p.ProductSize)
+                    .HasForeignKey(d => d.SizeId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ProductSize_Size");
+
+                entity.HasOne(d => d.ProductColor)
+                    .WithMany(p => p.ProductSize)
+                    .HasForeignKey(d => new { d.ProductId, d.ColorId })
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ProductSize_ProductColor");
+            });
+
+            modelBuilder.Entity<Role>(entity =>
+            {
+                entity.Property(e => e.RoleId).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Size>(entity =>
+            {
+                entity.Property(e => e.SizeId).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.HasOne(d => d.Status)
+                    .WithMany(p => p.Size)
+                    .HasForeignKey(d => d.StatusId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Size_Status");
+
+                entity.HasOne(d => d.TypeSize)
+                    .WithMany(p => p.Size)
+                    .HasForeignKey(d => d.TypeSizeId)
+                    .HasConstraintName("FK_Size_TypeSize");
+            });
+
+            modelBuilder.Entity<Status>(entity =>
+            {
+                entity.Property(e => e.StatusId).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<TypeProduct>(entity =>
+            {
+                entity.Property(e => e.TypeProductId).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.HasOne(d => d.ProductGender)
+                    .WithMany(p => p.TypeProduct)
+                    .HasForeignKey(d => d.ProductGenderId)
+                    .HasConstraintName("FK_TypeProduct_ProductGender");
+
+                entity.HasOne(d => d.Status)
+                    .WithMany(p => p.TypeProduct)
+                    .HasForeignKey(d => d.StatusId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_TypeProduct_Status");
+            });
+
+            modelBuilder.Entity<TypeSize>(entity =>
+            {
+                entity.Property(e => e.Id).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Vendor>(entity =>
+            {
+                entity.Property(e => e.VendorId).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.Address)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Phone)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
